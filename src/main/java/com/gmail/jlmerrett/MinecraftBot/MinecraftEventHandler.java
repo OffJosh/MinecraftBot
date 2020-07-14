ee136f4ac39b15b1fbf10b2f5b9b4a4622d1177e
BuildTools <unconfigured@null.spigotmc.org>
2020-07-14 17:27:32 +0100

    change to deathevent

--- src/main/java/com/gmail/jlmerrett/MinecraftBot/MinecraftEventHandler.java
+++ src/main/java/com/gmail/jlmerrett/MinecraftBot/MinecraftEventHandler.java
@@ -1,9 +1,15 @@
 package com.gmail.jlmerrett.MinecraftBot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class MinecraftEventHandler implements Listener {
 
@@ -14,8 +20,13 @@ public class MinecraftEventHandler implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent playerJoinEvent){
-        Player player = playerJoinEvent.getPlayer();
-        bot.botMessenger.sendMessage(player.getDisplayName() + " has joined the server.");
+    public void onPlayerDeath(PlayerDeathEvent playerDeathEvent){
+        Player player = playerDeathEvent.getEntity();
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective objective = board.getObjective("Deaths");
+        Score score = objective.getScore(player);
+        bot.botMessenger.sendMessage(player.getDisplayName() + " has died.");
+        bot.botMessenger.sendMessage(score.toString());
     }
 }
